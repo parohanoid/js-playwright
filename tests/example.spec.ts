caf8df58f0d22427f2f1209a68e7e58800a74a49
Rohan Panchal <rohanpanchal76@gmail.com>
2025-07-15 12:20:32 +0000

    added before each

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,25 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+test.describe('naviigate and check stuff', () => {
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://playwright.dev/');
+  })
+  
+  test('has title', async ({ page }) => {
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Playwright/);
+  });
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+  test('get started link', async ({ page }) => {
+
+    // Click the get started link.
+    await page.getByRole('link', { name: 'Get started' }).click();
+
+    // Expects page to have a heading with the name of Installation.
+    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  });
+
+})
